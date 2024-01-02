f301eb36e358055de0d629b7acd01d9d95ca80ff
Rivaldito <araquesebastian@gmail.com>
2024-01-01 22:27:43 -0400

    append and print list method added

--- linkedList.py
+++ linkedList.py
@@ -19,7 +19,25 @@ class LinkedList:
 
     def __init__(self, value):
         new_node    = Node(value)
-        self.head   = new_node
-        self.tail   = new_nod
+        self.head = new_node 
+        self.tail = new_node
         self.length = 1
     
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value) -> bool:
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.tail = new_node 
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
+        return True
---
+++ main.py
@@ -0,0 +1,8 @@
+from linkedList import LinkedList
+
+if __name__ == "__main__":
+
+    my_linked_list = LinkedList(1)
+    my_linked_list.print_list()
+    my_linked_list.append(2)
+    my_linked_list.print_list()
