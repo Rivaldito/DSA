41ffe65a68cb61d83a73019e30d9f0cf40b6db43
Rivaldito <araquesebastian@gmail.com>
2024-01-02 21:22:54 -0400

    LL add methods

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from linkedList import LinkedList
 if __name__ == "__main__":
 
     my_linked_list = LinkedList(1)
-    my_linked_list.print_list()
     my_linked_list.append(2)
     my_linked_list.print_list()
+
+    print("[]")
+    my_linked_list.prepend(4)
+
+    my_linked_list.print_list()
+
+    print("[1]")
+    print(my_linked_list.get(1))
+
+    print("[2]")
+
+    print(my_linked_list.pop_first())
+    my_linked_list.print_list()
+
+    #print(my_linked_list.get(1))
